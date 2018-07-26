1f0e328558cafc58a5d821785525f4611d18246f
Bryan Diola <bry.diola@gmail.com>
2018-07-26 14:57:41 +0800

    Add another line in textCode.java

--- src/testCode.java
+++ src/testCode.java
@@ -7,7 +7,12 @@ public class testCode {
         System.out.print("Enter a danm name: ");
         String pangalan = scan.nextLine();
 
-        System.out.print("The name you typed in is: "+pangalan);
+        System.out.print("Enter a number: ");
+        int number = scan.nextInt();
+
+        System.out.print("The name you typed in is: "+pangalan+"\n");
+        System.out.print("The number "+number+" you have entered is that.");
+
 
     }
 
