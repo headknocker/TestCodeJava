45b7ca2db490bdf16e6f32582586505332a19aab
Bryan Diola <bryan.diola@novare.com.hk>
2018-07-26 14:12:04 +0800

    Initialize

---
+++ src/testCode.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class testCode {
+
+    public static void main (String args[]){
+        Scanner scan = new Scanner (System.in);
+        System.out.print("Enter a danm name: ");
+        String pangalan = scan.nextLine();
+
+        System.out.print("The name you typed in is: "+pangalan);
+
+    }
+
+}
